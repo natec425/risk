39f23f0aa47e92208a3fe7f829536812026bbd91
natec <natec425@gmail.com>
2015-06-06 23:37:51 -0500

    Fixed a bug in choose(n, k). Implemented kth_n_combination.

--- util.py
+++ util.py
@@ -13,4 +13,22 @@ def integer_compositions(total, n):
 
 
 def choose(n, k):
-    return fact(n) * (fact(k) * fact(n - k))
+    if k > n:
+        return 0
+    return fact(n) / (fact(k) * fact(n - k))
+
+def kth_n_combination(items, n, k):
+    if n == 0:
+        return []
+    total_combos = choose(len(items), n)
+    if k >= total_combos:
+        raise IndexError("There aren't {} {}-combinations of {}.".format(
+            k+1, n, items
+        ))
+    combos_without_first = choose(len(items) - 1, n)
+    combos_with_first = total_combos - combos_without_first
+    if k < combos_with_first:
+        return [items[0]] + kth_n_combination(items[1:], n - 1, k)
+    else:
+        return kth_n_combination(items[1:], n, k - combos_with_first)
+
